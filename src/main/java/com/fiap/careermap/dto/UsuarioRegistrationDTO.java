31f118601d21f66013710e045994e8506dfda35f
lucasmirandaleite <lucas.leitee2005@gmail.com>
2025-11-21 17:29:52 -0300

    Update UsuarioRegistrationDTO.java

--- src/main/java/com/fiap/careermap/dto/UsuarioRegistrationDTO.java
+++ src/main/java/com/fiap/careermap/dto/UsuarioRegistrationDTO.java
@@ -1,21 +1,40 @@
 package com.fiap.careermap.dto;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-import lombok.Data;
-
-@Data
 public class UsuarioRegistrationDTO {
 
-    @NotBlank(message = "{usuario.nome.notblank}")
     private String nome;
-
-    @NotBlank(message = "{usuario.email.notblank}")
-    @Email(message = "{usuario.email.invalid}")
     private String email;
-
-    @NotBlank(message = "{usuario.senha.notblank}")
-    @Size(min = 6, message = "{usuario.senha.size}")
     private String senha;
+
+    public UsuarioRegistrationDTO() {}
+
+    public UsuarioRegistrationDTO(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
