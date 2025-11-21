c681dd8c09b5547663a46f656d3a0d64562972e7
lucasmirandaleite <lucas.leitee2005@gmail.com>
2025-11-21 16:28:21 -0300

    Refactor AuthController and update request mapping

--- src/main/java/com/fiap/careermap/controller/AuthController.java
+++ src/main/java/com/fiap/careermap/controller/AuthController.java
@@ -7,34 +7,32 @@ import com.fiap.careermap.model.Usuario;
 import com.fiap.careermap.service.TokenService;
 import com.fiap.careermap.service.UsuarioService;
 import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/auth")
 public class AuthController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
 
     @Autowired
-    private TokenService tokenService;
+    private UsuarioService usuarioService;
 
     @Autowired
-    private UsuarioService usuarioService;
+    private TokenService tokenService;
 
     @PostMapping("/login")
     public ResponseEntity<TokenResponseDTO> login(@RequestBody @Valid LoginDTO loginDTO) {
         UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(loginDTO.getEmail(), loginDTO.getSenha());
+                new UsernamePasswordAuthenticationToken(loginDTO.email(), loginDTO.senha());
 
         Authentication authentication = authenticationManager.authenticate(authenticationToken);
 
