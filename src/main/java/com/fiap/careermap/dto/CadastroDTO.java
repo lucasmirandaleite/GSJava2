f95f199c987b5a925e07969ae9e23a340c2bec63
lucasmirandaleite <lucas.leitee2005@gmail.com>
2025-11-22 01:21:45 -0300

    Create CadastroDTO.java

---
+++ src/main/java/com/fiap/careermap/dto/CadastroDTO.java
@@ -0,0 +1,31 @@
+package com.fiap.careermap.dto;
+
+import com.fiap.careermap.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CadastroDTO {
+    
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 3, max = 100, message = "Nome deve ter entre 3 e 100 caracteres")
+    private String nome;
+    
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
+    private String email;
+    
+    @NotBlank(message = "Senha é obrigatória")
+    @Size(min = 6, message = "Senha deve ter no mínimo 6 caracteres")
+    private String senha;
+    
+    // Role padrão será USER, mas pode ser enviado no JSON
+    private Role role = Role.USER;
+}
