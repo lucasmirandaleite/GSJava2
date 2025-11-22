ed52d281a4168115bbfc75b56623b221e4583938
lucasmirandaleite <lucas.leitee2005@gmail.com>
2025-11-21 22:44:02 -0300

    Create UsuarioDTO.java

---
+++ src/main/java/com/fiap/careermap/dto/UsuarioDTO.java
@@ -0,0 +1,15 @@
+package com.fiap.careermap.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioDTO {
+    private Long id;
+    private String email;
+    private String username;
+    private String role;
+}
