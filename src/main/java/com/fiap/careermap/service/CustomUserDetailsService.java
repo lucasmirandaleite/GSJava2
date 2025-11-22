4c5cc5a425adb6a3d2536dd79e5fb3234cfdab1c
lucasmirandaleite <lucas.leitee2005@gmail.com>
2025-11-22 00:29:46 -0300

    Create CustomUserDetailsService.java

---
+++ src/main/java/com/fiap/careermap/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.fiap.careermap.service;
+
+import com.fiap.careermap.model.Usuario;
+import com.fiap.careermap.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com email: " + email));
+
+        return User.builder()
+                .username(usuario.getEmail())
+                .password(usuario.getSenha()) // Certifique-se que a senha está codificada com BCrypt
+                .authorities("USER")
+                .build();
+    }
+}
