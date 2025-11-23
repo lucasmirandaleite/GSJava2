c98580372975f211c3f7681303f29cf4bf933cc9
lucasmirandaleite <lucas.leitee2005@gmail.com>
2025-11-22 22:42:10 -0300

    Update const.ts

--- client/shared/const.ts
+++ client/shared/const.ts
@@ -1,5 +1,5 @@
 export const COOKIE_NAME = "app_session_id";
 export const ONE_YEAR_MS = 1000 * 60 * 60 * 24 * 365;
 
-// Usando a variável de ambiente do Vite
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api/v1";
+// TEMPORÁRIO - PARA TESTE
+export const API_BASE_URL = "https://gsjava2-production.up.railway.app/api/v1";
