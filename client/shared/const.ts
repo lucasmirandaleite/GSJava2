e2e11b5155ea60966b35a2f3141c5436439dfd8a
lucasmirandaleite <lucas.leitee2005@gmail.com>
2025-11-22 22:26:48 -0300

    Update const.ts

--- client/shared/const.ts
+++ client/shared/const.ts
@@ -1,2 +1,5 @@
 export const COOKIE_NAME = "app_session_id";
 export const ONE_YEAR_MS = 1000 * 60 * 60 * 24 * 365;
+
+// ADICIONE ESTA LINHA:
+export const API_BASE_URL = "https://gsjava2-production.up.railway.app/api/v1";
